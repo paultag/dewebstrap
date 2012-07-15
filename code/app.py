64a010b96e3dba9742c42e9acc7997b7387f2eff
Paul Tagliamonte <tag@pault.ag>
2012-07-14 22:40:04 -0400

    More krufty kruft.

--- code/app.py
+++ code/app.py
@@ -1,3 +1,7 @@
+# Copyright (c) Paul Tagliamonte <paultag@debian.org>, 2012 under the terms
+# and conditions of the Expat license, a copy of which should be given to you
+# with the source of this application.
+
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
