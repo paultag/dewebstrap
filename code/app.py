8a064cbc1c29db2f9dc676e791778b3e4ca138b3
Paul Tagliamonte <tag@pault.ag>
2012-07-14 20:11:21 -0400

    Tweaking the setup a skitch

--- code/app.py
+++ code/app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', **{})
 
+@app.route("/about")
+def about():
+    return render_template('about.html', **{})
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
